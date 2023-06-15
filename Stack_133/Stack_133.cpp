53c4fcf7a77c39815a795fbecb3b0165e7cda08c
DaffaPutra7 <putradika3604@gmail.com>
2023-06-15 15:13:19 +0700

    .

--- Stack_133/Stack_133.cpp
+++ Stack_133/Stack_133.cpp
@@ -32,6 +32,15 @@ public:
 			return; //step 1.b
 		}
 
+		cout << "\nThe popped element is : " << stack_array[top] << endl; //step 2
+		top--; //step 3 decrement
+	}
+
+	//method for check if data is empty
+	bool empty() {
+		if (empty()) {
+			cout << "\nStack is empty." << endl;
+		}
 
 	}
 };
